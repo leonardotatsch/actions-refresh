1c6eac0ee3423877f230a72ac472c84ebfd7d9d6
Leonardo Tatsch Correa <leonardo.tatschcorrea@emeal.nttdata.com>
2024-09-24 14:35:41 +0100

    Refactor resource group variables and module configuration

--- tf/resource-group-variables.tf
+++ tf/resource-group-variables.tf
@@ -15,3 +15,13 @@ variable "LOCATION_SHARED" {
   description = "(Required) The name of the resource group location"
   default     = "westeurope"
 }
+
+locals {
+  rg_region1 = {
+    name = upper("rg-accelerator-we-rg01")
+    tags = {
+      hidden-title = "Test Resource Group"
+      Description  = "Resource group used for testing"
+    }
+  }
+}
--- tf/resource-group.tf
+++ tf/resource-group.tf
@@ -1,7 +1,7 @@
 module "resource_group_region1" {
   source = "../tf-modules/resource-group"
 
-  ENABLED  = var.ENABLED
+  ENABLED  = true #var.RG_ENABLED
   NAME     = local.rg_region1.name
   LOCATION = var.LOCATION_EU
   TAGS     = merge(local.global_tags, local.rg_region1.tags)
