b2eb2bb062f60fd3b25b718c583c2ca61adc2d29
Leonardo Tatsch Correa <leonardo.tatschcorrea@emeal.nttdata.com>
2024-09-24 14:53:35 +0100

    Refactor variable names and default values in __common-variables__.tf and resource-group-variables.tf

--- tf/__common-variables__.tf
+++ tf/__common-variables__.tf
@@ -1,8 +1,31 @@
+variable "PROJECT_NAME" {
+  type = string
+  default = "ACCELERATOR"
+}
+
+variable "LOCATION_EU" {
+  type        = string
+  description = "(Required) The name of the resource group location"
+  default     = "westeurope"
+}
+
+variable "LOCATION_US" {
+  type        = string
+  description = "(Required) The name of the resource group location"
+  default     = "centralus"
+}
+
+variable "LOCATION_SHARED" {
+  type        = string
+  description = "(Required) The name of the resource group location"
+  default     = "westeurope"
+}
+
 locals {
 
   global_tags = {
-    BusinessOwner = "NTTDATA" #var.BUSINESSOWNER_TAG
+    BusinessOwner = "CHANGEME" #var.BUSINESSOWNER_TAG
     Environment   = "SANDBOX" #var.ENVIRONMENT
-    ServiceModel  = "Managed by NTTDATA" #var.SERVICEMODEL
+    ServiceModel  = "Managed by CHANGEME" #var.SERVICEMODEL
   }
 }
--- tf/resource-group-variables.tf
+++ tf/resource-group-variables.tf
@@ -1,24 +1,6 @@
-variable "LOCATION_EU" {
-  type        = string
-  description = "(Required) The name of the resource group location"
-  default     = "westeurope"
-}
-
-variable "LOCATION_US" {
-  type        = string
-  description = "(Required) The name of the resource group location"
-  default     = "centralus"
-}
-
-variable "LOCATION_SHARED" {
-  type        = string
-  description = "(Required) The name of the resource group location"
-  default     = "westeurope"
-}
-
 locals {
   rg_region1 = {
-    name = upper("rg-accelerator-we-rg01")
+    name = upper("rg-${var.PROJECT_NAME}-we-rg01")
     tags = {
       hidden-title = "Test Resource Group"
       Description  = "Resource group used for testing"
