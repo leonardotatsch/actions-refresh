8580162c15b849397ade4724f048c71a2a0f7fe5
Leonardo Tatsch Correa <leonardo.tatschcorrea@emeal.nttdata.com>
2024-09-27 17:35:36 +0100

    Refactor subnet.tf: Update private_endpoint_network_policies attribute to use boolean values

--- tf-modules/networking/subnet/subnet.tf
+++ tf-modules/networking/subnet/subnet.tf
@@ -4,7 +4,7 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name                           = var.RG_NAME
   virtual_network_name                          = var.VNET_NAME
   address_prefixes                              = var.SUBNET_ADDRESS_SPACE
-  private_endpoint_network_policies_enabled     = var.END_NETWORK_POLICIES_ENABLED
+  private_endpoint_network_policies             = var.END_NETWORK_POLICIES_ENABLED
   private_link_service_network_policies_enabled = var.NETWORK_POLICIES_ENABLED
   service_endpoints                             = var.SERVICE_ENDPOINTS
 
@@ -20,6 +20,6 @@ resource "azurerm_subnet" "subnet" {
   }
 }
 
-## In order to deploy a Private Link Endpoint on a given subnet, you must set the private_endpoint_network_policies_enabled attribute to false.
+## In order to deploy a Private Link Endpoint on a given subnet, you must set the private_endpoint_network_policies attribute to false.
 ## In order to deploy a Private Link Service on a given subnet, you must set the private_link_service_network_policies_enabled attribute to false.
 ## To utilize network policies like UDR and NSG support, network policy support must be enabled for the subnet.
