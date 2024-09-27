c84d6fb4d604f488015d8e41a8e4456fd0b59430
Leonardo Tatsch Correa <leonardo.tatschcorrea@emeal.nttdata.com>
2024-09-27 16:53:23 +0100

    Refactor vnet module: Update variable names for clarity

--- tf/vnet.tf
+++ tf/vnet.tf
@@ -2,10 +2,10 @@ module "vnet_spoke" {
   source   = "../tf-modules/networking/vnet"
   for_each = local.vnet_spoke
 
-  ENABLED       = each.value.enabled
+  VNET_ENABLED  = each.value.enabled
   NAME          = each.value.name
   LOCATION      = each.value.location
   RG_NAME       = each.value.rg_name
-  ADDRESS_SPACE = each.value.vnet_address_space
+  VNET_ADDRESS_SPACE = each.value.vnet_address_space
   TAGS          = merge(local.global_tags, each.value.tags)
 }
